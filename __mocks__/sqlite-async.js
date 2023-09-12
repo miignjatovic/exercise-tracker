6f17a3a484fbf1e763a5d3fc020af0475d8caa0a
miignjatovic <mignjatovic@griddynamics.com>
2023-09-12 11:14:22 +0200

    db connection fix

--- __mocks__/sqlite-async.js
+++ __mocks__/sqlite-async.js
@@ -2,9 +2,9 @@
 
 export const Database = {
     open: jest.fn(() => Promise.resolve({
-    run: jest.fn(() => Promise.resolve({ lastID: 1 })),
-    all: jest.fn(() => Promise.resolve([])),
-    close: jest.fn(() => Promise.resolve())
-  }))
+        run: jest.fn(() => Promise.resolve({ lastID: 1 })),
+        all: jest.fn(() => Promise.resolve([])),
+        close: jest.fn(() => Promise.resolve())
+    }))
 }
   
--- index.js
+++ index.js
@@ -6,35 +6,40 @@ import morgan from 'morgan'
 import routes from './api/routes.js'
 import { openDbConnection, closeDbConnection, useDb } from './api/db.middleware.js'
 
-// get db connection
-const db = await openDbConnection()
-
 // create express app
 const app = express()
 
-// setup middleware
-app.use(useDb(db))
-app.use(cors())
-app.use(morgan('dev'))
-app.use(bp.json())
-app.use(bp.urlencoded({ extended: true }))
-app.use(express.static('public'))
-
-// root route
-app.get('/', (req, res) => {
-    res.sendFile(fileURLToPath(new URL('views/index.html', import.meta.url)))
+// get db connection
+openDbConnection().then(db => {
+    // setup middleware
+    app.use(useDb(db))
+    app.use(cors())
+    app.use(morgan('dev'))
+    app.use(bp.json())
+    app.use(bp.urlencoded({ extended: true }))
+    app.use(express.static('public'))
+    
+    // root route
+    app.get('/', (req, res) => {
+        res.sendFile(fileURLToPath(new URL('views/index.html', import.meta.url)))
+    })
+    
+    // api routes
+    app.use('/api', routes)
+    
+    // if testing use a dynamic port because of how jest runs tests
+    const PORT = process.env.NODE_ENV === 'test' ? 0 : 3000
+    
+    const listener = app.listen(PORT, () => {
+        console.log('Your app is listening on port ' + listener.address().port)
+    })
+    
+    closeDbConnection(db)
+}).catch(err => {
+    console.log('DB connection error:', err)
+    process.exit(1)
 })
 
-// api routes
-app.use('/api', routes)
 
-// if testing use a dynamic port because of how jest runs tests
-const PORT = process.env.NODE_ENV === 'test' ? 0 : 3000
-
-const listener = app.listen(PORT, () => {
-    console.log('Your app is listening on port ' + listener.address().port)
-})
-
-closeDbConnection(db)
 
 export default app
